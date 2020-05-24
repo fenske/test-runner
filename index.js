0bfc2fc27cfc966f6b33c1d78e34da023835b376
Anton Fenske <anton@fenske.xyz>
2020-05-24 14:52:54 +0200

    First version

--- index.js
+++ index.js
@@ -4,20 +4,25 @@ const axios = require('axios');
 const newman = require('newman'); // require newman in your project
 
 // call newman.run to pass `options` object and wait for callback
+//TODO:
+// - might need to add the test code to the results for each assertion. That way you can present the test that didn't work
+// - Add description to each test. Use name as a human readable identifier, e.g. scenario-1-update-and-fetch-balance. Use it to bind the test definition coming
+// from the collection part of the summary with the corresponding execution
+// Pass in collection.info.name to identify the challenge
 newman.run({
   collection: require('./api-tests.json'),
   reporters: 'cli'
 }, function (err, summary) {
   let executions = summary.run.executions.map(transformExecution);
 
-  let result = {
+  let results = {
     stats: summary.run.stats,
     timings: summary.run.timings,
     executions: executions
   }
-  axios.post('http://localhost:3000/api/segment', {
-    user: 'd953aa85-9fe3-4d88-a66f-397875f42bde',
-    results: result
+  axios.post('https://test-server-rho.now.sh/api/accept-results', {
+    user: 'a953aa85-9fe3-4d88-a66f-397875f42bde',
+    results: results
   })
     .then(function (response) {
       console.log("Here comes the response: 🏄")
@@ -28,7 +33,7 @@ newman.run({
     });
   // Use token
 
-  console.log(JSON.stringify(result));
+  console.log(JSON.stringify(results));
 });
 
 function transformExecution(exec) {
