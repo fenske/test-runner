0383f7dc1939e9185e3d4fa71f2f453a0a602115
Anton Fenske <anton@fenske.xyz>
2020-05-27 06:12:20 +0200

    Updated todo.

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const axios = require('axios');
 
 const newman = require('newman'); // require newman in your project
 
-// call newman.run to pass `options` object and wait for callback
-//TODO:
+//TODO: For tomorrow
+// Send basic challenge stats 5 mins
+// Make the script accept a api_key as a param. 10 mins
+// Publish an rpm 20 mins.
+
+
+//TODO: Next iteration
+// 0. Super quickly, 10 mins. Understand whether you can run against a remote collection.
+// 1. Identify what parts of the test summary should be sent. 10 mins.
 // - might need to add the test code to the results for each assertion. That way you can present the test that didn't work
+// Pass in collection.info.name to identify the challenge
+// 2. Update the collection file accordingly. 15 mins
 // - Add description to each test. Use name as a human readable identifier, e.g. scenario-1-update-and-fetch-balance. Use it to bind the test definition coming
 // from the collection part of the summary with the corresponding execution
-// Pass in collection.info.name to identify the challenge
+// 3. Publish an rpm 20 mins
 newman.run({
   collection: require('./api-tests.json'),
   reporters: 'cli'
