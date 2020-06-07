9859227376ee57d5b96eff4064171064829f3596
Anton Fenske <anton@fenske.xyz>
2020-06-07 07:58:36 +0200

    Fix summary

--- index.js
+++ index.js
@@ -39,9 +39,11 @@ try {
       })
       .catch(function (error) {
       });
+
+    core.setOutput("summary", summary);
   });
 
-  core.setOutput("summary", summary);
+  core.setOutput("summary", "");
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
